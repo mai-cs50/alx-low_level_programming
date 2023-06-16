a43e629e4f9551a18115cfc184fba3b0d50ff435
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-16 09:04:58 -0700

    w

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description:
+ *
+ *Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	if (n < 10, n++;)
+		printf("%i", n)
+
+	return (0);
+}
