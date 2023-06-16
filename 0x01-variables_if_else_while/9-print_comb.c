a9dd3729f615ae5ff7ca84f3d84e045b4a3fd691
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-16 13:38:27 -0700

    e

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description:
+ *
+ *Return: 0 (success)
+ *
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+		if (x < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
