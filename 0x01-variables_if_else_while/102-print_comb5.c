087275ff371ed6bed478e8b8084ee44234d30447
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-17 09:21:45 -0700

    d

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description:
+ *
+ *Return: 0 (success)
+ *
+*/
+
+int main(void)
+{
+	int x = 0;
+	int y;
+
+	while (x <= 99)
+	{
+		x = y;
+		while (y <= 99)
+		{
+			if (y != x)
+			{
+				putchar((x / 10) + '0');
+				putchar((x % 10) + '0');
+				putchar(' ');
+				putchar((y / 10) + '0');
+				putchar((y % 10) + '0');
+
+				if (x != 99 || y != 99)
+				{
+					putchar(',')
+					putchar(' ')
+				}
+			}
+			y++;
+		}
+		x++;
+	}
+	putchar('\n')
+
+	return (0);
+}
