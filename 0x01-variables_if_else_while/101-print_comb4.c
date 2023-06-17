486c798bffde9fff296c5d3ac205f47198fea2be
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-17 08:27:38 -0700

    d

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description:
+ *
+ *Return: 0 (success)
+ *
+*/
+
+int main(void)
+{
+        int x = 0;
+        int y = 0;
+	int z = 0;
+
+	while (x < 8)
+	{
+        	while (y < 9)
+        	{
+			while (y <= x)
+			{
+				y++;
+			}
+			while (z <= x || z <= y)
+			{
+				z++;
+			}
+
+                        putchar(x + '0');
+                        putchar(y + '0');
+			putchar(z + '0');
+			if (x + y != 24)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
