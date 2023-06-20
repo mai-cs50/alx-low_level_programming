3c1917148c13ac884372567d9e5aab4033ce9f48
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-20 13:26:32 -0700

    d

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,6 +12,11 @@ void times_table(void)
 		for (y = 0; y <= 9; y++)
 		{
 			multi = x * y;
+			if (multi > 9)
+			{
+				_putchar((multi / 10) + '0');
+				_putchar((multi % 10) + '0');
+			}
 			_putchar(multi + 48);
 				if (y == 9)
 				{
