664fef8790ef8a4b54fb460dce6471c400ec4842
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-20 08:01:35 -0700

    i

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * print_alphabet - putchar function
+ *
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 'z', ch++)
+		-putchar(ch);
+	-putchar('\n');
+}
+
