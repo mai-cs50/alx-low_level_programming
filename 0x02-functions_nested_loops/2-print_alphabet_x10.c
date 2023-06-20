5e95ad97cedf26f20f2a085f561614cec6f1f69c
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-20 08:31:02 -0700

    w

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,10 @@ void print_alphabet_x10(void)
 {
 	int line, ch;
 
-	for (line = 0; line <= 9; line++);
+	for (line = 0; line <= 9; line++)
 	{
-		for (ch = 'a'; ch <= 'z'; ch++);
-			-putchar(ch);
-		-putchar('\n');
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
 	}
 }
