1a738546ed12bb98baaf0ad42aaa47658c489def
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-20 14:07:45 -0700

    d

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,20 +1,16 @@
 #include "main.h"
 /**
+ * add - add two int
  *
+ * @x: first
+ * @y: second
  *
- *
- *
- *
- *
- *
+ * Rturn: sum of x and y
  *
 */
-int add(int, int)
+int add(int x, int y)
 {
+	int sum = x + y;
 
-
-
-
-
-
+	return (sum);
 }
