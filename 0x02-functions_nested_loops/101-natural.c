18475ff6771b1479e196377fa61d317e0da81ade
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-21 08:08:40 -0700

    r

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: sum
+ *
+ * Return: always 0 (success)
+ *
+*/
+int main(void)
+{
+	int n, sum;
+
+	for (n = 0; n <= 1024; n++)
+	{
+		if (n % 5 == 0 || n % 3)
+			sum = sum + n;
+	}
+	printf("%d\n, sum");
+	return (0);
+}
