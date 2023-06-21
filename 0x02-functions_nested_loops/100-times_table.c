90d2284120f58b2e9d3324cc472958a4230e36e2
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-21 07:01:55 -0700

    r

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+*/
+void print_times_table(int n)
+{
+	int x, y, multi;
+
+	for (x = 0; x <= 15; x++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (y = 1; y <= 9; y++)
+		{
+			multi = x * y;
+			if (multi <= 9)
+			{
+				_putchar(' ');
+			}
+			if (multi <= 99)
+			{
+				_putchar(' ');
+			}
+			if (multi > 99)
+			{
+				_putchar((multi / 100) + '0');
+				_putchar(((multi / 10) % 10) + '0')
+			}
+			else if (multi <=10 && multi >= 99)
+				_putchar((multi / 10) + '0');
+			_putchar((multi % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
