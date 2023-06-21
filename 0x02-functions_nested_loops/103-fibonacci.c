e689dea402185c6a8d0664b709fa9edab8f25631
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-21 09:50:28 -0700

    e

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description:
+ *
+ * Return: 0 (success)
+ *
+*/
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	float tsum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2) == 0)
+			tsum = tsum + sum;
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+
+	printf("%.0f\n", tsum)
+
+	return (0);
+}
