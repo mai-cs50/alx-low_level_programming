3944fb9fb5cc0d52a8864ef0c6a11445f36b4013
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-22 09:49:05 -0700

    f

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - 0 to 9
+ *
+ * Return: always 0
+ *
+*/
+void print_most_numbers(void)
+{
+	int num ;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
