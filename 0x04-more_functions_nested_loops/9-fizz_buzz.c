6ed9fc33e33c0c0ac34f07c237df515469365aee
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-23 13:08:28 -0700

    e

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,17 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0)
-			printf("Fizz ");
-		if (num % 5 == 0)
-			printf("Buzz ");
-		if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz ");
+		if (num % 3 == 0 && !(num % 5 == 0))
+			printf("Fizz");
+		else if (num % 5 == 0 && !(num % 3 == 0))
+			printf("Buzz");
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
 		else
-			printf("%d ", num);
+			printf("%d", num);
+		if (num != 100)
+			printf(" ");
+		else
+			printf("\n");
 	}
-	printf("\n");
 }
