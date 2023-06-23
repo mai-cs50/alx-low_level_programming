8ea26dea5c763114d160cfa74578db5c4772dd4b
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-23 12:35:33 -0700

    e

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,11 +10,15 @@ void print_square(int size)
 {
 	int row, col;
 
-	for  (row = 1; row <= size ; row++)
-	{
-		for (col = 1; col <= size ; col++)
-			_putchar('#');
-		_putchar('\n');
-	}
+	for (size = 0)
+		_puchar('\n')
+	else
+
+		for  (row = 1; row <= size ; row++)
+		{
+			for (col = 1; col <= size ; col++)
+				_putchar('#');
+			_putchar('\n');
+		}
 
 }
