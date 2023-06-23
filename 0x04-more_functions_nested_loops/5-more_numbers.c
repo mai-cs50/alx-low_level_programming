a76d0ed214d8934aafd26c49af09b0722e1fc28e
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-23 10:32:20 -0700

    e

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h0"
+/**
+ * more_numbers - print 0 - 14
+ *
+ * Return: always (0)
+ *
+*/
+void more_numbers(void)
+{
+	int x, y, num;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 14; y++)
+			y = num;
+		{
+			if (y > 9)
+				_putchar(1 + 48);
+				num = y % 10;
+
+			_putchar(num + 48);
+		}
+		putchar('\n');
+	}
+}
