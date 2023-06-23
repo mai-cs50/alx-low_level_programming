cc66b4c933abd77941f4d46ee1cd09231fd6dce9
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-23 12:14:48 -0700

    e

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main"
+/**
+ * print_diagonal - print line
+ *
+ * @n: number of /
+ *
+*/
+void print_diagonal(int n)
+{
+	int l, s;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (l = 1; l <= n; l++)
+		{
+			for (s = 1; s <= l; s++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
