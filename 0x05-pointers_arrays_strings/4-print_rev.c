ab5baebfbf0f5c13ef9a167fa2e20dce2c15f20f
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-26 08:55:47 -0700

    d

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - length of
+ *
+ * @s: char
+ *
+ * Return: 0
+ *
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar('s[i]');
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+*/
+void rev_string(char *s)
+{
+
+}
