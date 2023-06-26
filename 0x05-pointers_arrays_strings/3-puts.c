0268a979d99f0986fd9471db1cbe6435ed68ab0d
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-26 02:55:59 -0700

    d

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - print
+ *
+ * @str: char
+ *
+ * Return: 0
+ *
+*/
+void _puts(char *str)
+{
+	while (*str != '\n')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
