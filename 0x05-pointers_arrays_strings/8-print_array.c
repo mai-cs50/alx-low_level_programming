d1772e25c725f50509805d4c00c04de69e673faf
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-26 13:20:22 -0700

    d

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_array - print
+ *
+ * @a: integer
+ * @n: integer
+ *
+ * Retrun: 0
+ *
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, "a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
+}
+
