4ee7295ddcfd7c6a3f95c81e07f3d687fbaff8ad
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-26 09:11:18 -0700

    d

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,22 @@
 #include "main.h"
 /**
+ * rev_string - revers
  *
+ * @s: char
  *
- *
- *
- *
- *
+ * Return: 0
  *
 */
 void rev_string(char *s)
 {
+	int i, len, temp;
 
+	len = strlen(s);
+
+	for (i = 0; i < len/2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
 }
