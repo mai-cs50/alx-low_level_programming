ce4bf68d9a66e0d27703d468951bd9f429c05135
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-26 02:05:36 -0700

    s

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - length of char
+ *
+ * @s: char
+ *
+ * Return: 0
+ *
+*/
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; c != '\0'; c++)
+		c++;
+	return (0);
+}
