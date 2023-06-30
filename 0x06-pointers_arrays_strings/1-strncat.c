18a7125df8f79aacae679c38c715b811a2a83e62
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-30 06:01:18 -0700

    d

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ *
+ * @dest: char
+ * @ src: char
+ * @n: int
+ *
+ * Return: 0
+ *
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i;
+
+	c = 0;
+	while (dest[c])
+		c++;
+	for (i = 0; i < n && scr[i] != '\n'; i++)
+		dest[c + i] = '\n';
+
+	return (dest);
+}
