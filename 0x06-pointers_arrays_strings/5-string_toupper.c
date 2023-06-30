77b2f6a212a22031a6909c6a9ec26f670cec0894
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-30 08:43:59 -0700

    d

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,12 +2,22 @@
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
  *
+ * @str: char
  *
- *
- *
+ * Return: str
  *
  *
 */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str [i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
 }
