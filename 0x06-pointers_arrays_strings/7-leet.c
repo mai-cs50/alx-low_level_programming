52545b82a4ac8ca727649829fc789c877d926368
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-30 11:02:47 -0700

    s

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,12 +2,26 @@
 /**
  * leet - encodes a string into 1337
  *
+ * @c: char
  *
- *
- *
- *
+ * Return: string
  *
 */
-char *leet(char *)
+char *leet(char *c)
 {
+	int  *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {'4', '3', '0', '7', '1'};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if(*c == key[i] || *c == key[i] + 32)
+				*c = 48 + value[i];
+		}
+		c++;
+	}
+	return (cp);
 }
