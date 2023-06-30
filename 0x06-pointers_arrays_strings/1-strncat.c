f71bab2f4abd731e0b964b402f208edcd988805d
mai-cs50 <maimohamed.1992@yahoo.com>
2023-06-30 06:05:26 -0700

    d

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,9 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[c])
 		c++;
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[c + i] = '\0';
+		dest[c + i] = src[i];
+
+	dest[c + i] = '\0';
 
 	return (dest);
 }
