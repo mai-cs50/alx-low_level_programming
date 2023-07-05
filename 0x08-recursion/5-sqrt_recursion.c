6cef187e111cf3b736828225891c704d28ea7ba6
mai-cs50 <maimohamed.1992@yahoo.com>
2023-07-05 14:39:09 -0700

    s

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,7 +22,7 @@ int _square_recursion(int n)
 */
 int square(int n, int val)
 {
-	if (val * val == 0)
+	if (val * val == n)
 		return (val);
 	else if (val * val < n)
 		return (square (n, val + 1));
