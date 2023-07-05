a177887516113685e7866170cb9de14c7d002fe7
mai-cs50 <maimohamed.1992@yahoo.com>
2023-07-05 12:42:38 -0700

    s

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: char
+ *
+ * Return: 0
+ *
+*/
+int _strlen_recursion(char *s)
+{
+	int r = 0;
+
+	if (*s > '\0')
+		r += _strlen_recursion(s + 1) + 1;
+	return(r);
+}
