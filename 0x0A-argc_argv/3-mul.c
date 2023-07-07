76fb656886705e2cc37e350b95afbad283c45489
mai-cs50 <maimohamed.1992@yahoo.com>
2023-07-07 10:22:23 -0700

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,10 +1,25 @@
 #include <main.h>
+#include <stlib.h>
 /**
  * main - Entry point
  *
+ * @argc: int
+ * @argv: char
  *
- *
- *
+ * Return: 0
  *
 */
-int main
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1] * atoi(argv[2]));
+		printf("%d\n", sum);
+	}
+	else {
+		printf("error\n");
+		return (1);
+	}
+}
