25cbe044ea93320752524b8116f716f0214c32f3
mai-cs50 <maimohamed.1992@yahoo.com>
2023-07-07 10:46:44 -0700

    s

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,10 +1,26 @@
-#include <main.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  *
+ * @argc: int
+ * @argv: char
  *
- *
- *
+ * Return: 0
  *
 */
-int main
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("error\n"), 1);
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
