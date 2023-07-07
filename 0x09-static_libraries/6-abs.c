a2459dcc4c5debd95bbfb11ca2520a71dd912677
mai-cs50 <maimohamed.1992@yahoo.com>
2023-07-07 08:13:10 -0700

    libmy

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ *
+ * @n: takes in integer type input in function
+ *
+ * Return: always 0 (successs)
+ *
+*/
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1 * n);
+	return (n);
+}
