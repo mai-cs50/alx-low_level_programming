afb438e935dc80ccb0ebc1a9b0e0ddd25d905abd
mai-cs50 <maimohamed.1992@yahoo.com>
2023-07-07 10:10:42 -0700

    s

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,11 @@
+#include <main.h>
+/**
+ * main - Entry point
+ *
+ *
+ *
+ *
+ *
+ *
+*/
+int main
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * main - Entry point
+ *
+ * @argc: int
+ * @argv: char
+ *
+ * Return: 0
+ *
+*/
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+	 while (argc--)
+	 {
+		 printf("%s\n", argv[i]);
+			 i++;
+	 }
+	 return (0);
+}
