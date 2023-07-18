c0c848fbf715b12f4455cbc2aeebcb3c1f685d1f
mai-cs50 <maimohamed.1992@yahoo.com>
2023-07-18 08:39:44 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog struct
+ *
+ * @name:
+ * @age:
+ * @owner:
+ *
+ * Description: a dog struct
+ *
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
