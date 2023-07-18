d0626f11a1ba0c69d86b10d744832768424ad054
mai-cs50 <maimohamed.1992@yahoo.com>
2023-07-18 08:45:36 -0700

    a

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog struct
+ *
+ * @name: char
+ * @age: float
+ * @owner: char
+ *
+ * Description: a dog struct
+ *
+*/
+struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
+
