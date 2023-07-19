d29223918bcd2354edf22e0dfc781f857f8d1b21
mai-cs50 <maimohamed.1992@yahoo.com>
2023-07-19 15:17:02 -0700

    s

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,24 @@
+#include "3-calc.h"
+/**
+ * main - check code
+ * @argc: n
+ * @argv: argument
+ *
+ * Return: 0
+ *
+*/
+int main(int argc, int **argv)
+{
+	int (*op_func)(int , int), a, b;
+
+	if (argc != 4)
+		printf("error\n"), exit(98);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+		printf("error\n"), exit(99);
+	printf("%d\n", op_func(a, b));
+	return (0);
+
+}
