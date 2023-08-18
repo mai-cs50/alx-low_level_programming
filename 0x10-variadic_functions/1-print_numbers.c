f02f8946092a21a3d09e23bfdfb3fb2cf3942a08
mai-cs50 <maimohamed.1992@yahoo.com>
2023-08-18 10:59:52 -0700

    g

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,1 +1,27 @@
-dsffdgd
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print num
+ *
+ * @separator:
+ * @n: int
+ * @...: int
+ *
+ * Return: void
+ *
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = n;
+	va_list (ap);
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	while (i--)
+		printf("%d%s", va_arg(ap, int)), i ? (separator : "") : ("\n");
+	va_end(ap);
+}
