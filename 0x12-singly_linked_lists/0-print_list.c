8a021c9ec838a9208e1a0ec78ea6335d66c37983
mai-cs50 <maimohamed.1992@yahoo.com>
2023-08-25 13:49:12 -0700

    r

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * _strlen - string length
+ * @s: count
+ *
+ * Return: int string length
+*/
+int _strlen(char *c)
+{
+	int i = 0;
+
+	if (!c)
+		return (0);
+	while (*c++)
+		i++;
+	return (i);
+}
+/**
+ * rint_list - rint_list
+ * @l: pointer
+ *
+ * Return: list
+*/
+size_t print_list(const list_t *l)
+{
+	size_t i = 0;
+
+	while (l)
+	{
+		printf("[%d] %s\n", _strlen(l->str), h->str ? h->str : "(nill)");
+	}
+	return (i);
+}
+
