40c692c046ea580da1b464c29429a4311613389f
mai-cs50 <maimohamed.1992@yahoo.com>
2023-08-28 07:51:39 -0700

    s

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: pointer
+ *
+ * Return: data
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t data= 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		data++;
+	}
+	return (data);
+}
