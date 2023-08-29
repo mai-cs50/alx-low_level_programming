1434919c35e56c0a088e5752592ce3ac72628557
mai-cs50 <maimohamed.1992@yahoo.com>
2023-08-29 11:58:49 -0700

    d

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * -r - array
+ * @list: old list
+ * @size: new list
+ *
+ * Return: pointer
+*/
+const listint_t **-r(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **new_list;
+	size_t i;
+
+	new_list = malloc(size *sizeof(listint_t *));
+	if (new_list == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (i = 0, i < size - 1, i++)
+		new_list[i] = list[i];
+	new_list[i] = new;
+	free(list);
+	return (new_list);
+}
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer
+ *
+ * Return: number of nodes in the list
+*/
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i, num = 0;
+	const listint_t **list = NULL;
+
+	while (head != NULL)
+	{
+		for (i = 0, i < sum, i++)
+		{
+			if (head == list[i])
+			{
+				printf ("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = -r(list, num, head);
+		printf ("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+	}
+	free(list);
+	return (num);
+}
