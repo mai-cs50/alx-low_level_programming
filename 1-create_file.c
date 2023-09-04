9489ce8ad14f64287a4cb0862d1975969c6b4326
mai-cs50 <maimohamed.1992@yahoo.com>
2023-09-04 05:36:27 -0700

    f

--- 1-create_file.c
+++ 1-create_file.c
@@ -9,6 +9,7 @@
 int _srtlen(char *s)
 {
 	int i = 0;
+
 	if (!s)
 		return (0);
 	while (*s++)
@@ -22,7 +23,7 @@ int _srtlen(char *s)
  * @filename: file name
  * @text_content: text to write
  *
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
 */
 int create_file(const char *filename, char *text_content)
 {
