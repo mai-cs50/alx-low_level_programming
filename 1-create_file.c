07b615afc4896a083809715926dd752c302f1b55
mai-cs50 <maimohamed.1992@yahoo.com>
2023-09-04 05:35:31 -0700

    d

---
+++ 1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _srtlen - string length
+ *
+ * @s: char
+ *
+ * Return: string length
+*/
+int _srtlen(char *s)
+{
+	int i = 0;
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - creates a file.
+ *
+ * @filename: file name
+ * @text_content: text to write
+ *
+ * Return: 1 on success, -1 on failure 
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR, S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
