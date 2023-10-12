40c93e36f0ed671374c8526ad91423bef412207e
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-12 10:14:54 -0700

    s

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: pointer to head node
+ *
+ * Return: void
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
