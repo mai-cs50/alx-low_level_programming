e47a5616df9839649310c2f826044d90c11603f0
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-12 09:50:49 -0700

    s

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,12 +14,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (new ? free(new), NULL : NULL);
 
 	new->n = n;
-	new->prev = NULL;
+	new->next = NULL;
 
 	if (!*head)
 	{
 		*head = new;
-		new->next = NULL;
+		new->prev = NULL;
 	}
 	else
 	{
