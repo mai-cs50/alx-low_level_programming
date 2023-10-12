49ad124be49183fdd6666421296808392171169f
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-12 11:45:18 -0700

    s

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 	}
 	if (tmp->next == NULL)
-		return (add_nodeint_end(h, n));
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
