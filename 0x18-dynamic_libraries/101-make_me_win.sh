ce057f7dc36c3c36c97866449c9b1aa520eeaccc
mai-cs50 <maimohamed.1992@yahoo.com>
2023-10-16 13:33:55 -0700

    s

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp http://github.com/mai-cs50/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp http://github.com/mai-cs50/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
