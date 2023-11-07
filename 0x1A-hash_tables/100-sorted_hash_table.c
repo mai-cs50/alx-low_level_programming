8c3d7db6953e4d6c95714a02ac8d8a7becd8d9fb
mai-cs50 <maimohamed.1992@yahoo.com>
2023-11-07 01:03:43 -0800

    s

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -181,3 +181,24 @@ void shash_table_print(const shash_table_t *ht)
 		tmp = tmp->next;
 	}
 }
+void shash_table_delete(shash_table_t *ht)
+{
+	unsigned long int i;
+	shash_node_t *next;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		next = ht->array[i]->next;
+		free(ht->array[i]->key);
+		free(ht->array[i]->value);
+		free(ht->array[i]);
+		ht->array[i] = next;
+	}
+}
+free(ht->array);
+ht->array = NULL;
+ht->shead = ht->stail = NULL;
+ht->size = 0;
+free(ht);
