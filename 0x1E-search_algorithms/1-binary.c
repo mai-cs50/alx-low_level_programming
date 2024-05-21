6c1a818dee95881a8dff3a1d9a0e296b736a0d85
mai-cs50 <maimohamed.1992@yahoo.com>
2024-05-21 12:00:12 -0700

    d

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,47 @@
+#include "search_algos.h"
+/**
+ * linear_search - search
+ * @array: the array
+ * @size: size of array
+ * @value: target value
+ * Return: Always EXIT_SUCCESS
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int i;
+
+	for (i = l; i <= r; i++)
+	{
+		printf("%d", array [i]);
+		if (i < r)
+			printf(", ");
+	}
+	printf ("\n");
+}
+/**
+ * linear_search - search
+ * @array: the array
+ * @size: size of array
+ * @value: target value
+ * Return: Always EXIT_SUCCESS
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int l = 0, r = size - 1, m
+
+	if (!array || size == 0)
+                return (-1);
+
+        while (l <= r)
+        {
+                printf("Searching in array: ");
+		print_array(array, l, r);
+		m = l + (r - 1) / 2;
+
+		if (array[m] < value)
+			l = m + 1;
+		else
+			return (m);
+        }
+        return (-1);
+}
