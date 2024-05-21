43f21ce08709b70d1245347f1327f7c9db65abef
mai-cs50 <maimohamed.1992@yahoo.com>
2024-05-21 11:03:06 -0700

    a

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,6 @@
+#include "serch_algos.h"
+/**function that searches for a value in an array of integers
+using the Linear search algorithm
+*/
+int linear_search(int *array, size_t size, int value)
+
