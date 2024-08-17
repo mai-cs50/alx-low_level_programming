1474c545aa234ab79fb88a7690200a6cf7799162
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 03:57:49 -0700

    d

--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -1,22 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - check the code
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: Always 0.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-int main(int ac, char **av)
+int create_file(const char *filename, char *text_content)
 {
-    int res;
+	int o, w, len = 0;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
