7afcbde0d195829ed486edd06aef56767ae04ead
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 00:13:56 -0700

    d

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -18,23 +18,15 @@ int main(int argc, char *argv[])
 
 		for (i = 0; i < 5; i++)
 		{
-			if (money >= c[i])
-			{
-				lc += money / c[i];
-				money = money % c[i];
-				if (money % c[i] == 0)
-				{
-					break;
-				}
-			}
+			lc += money / c[i];
+			money %= c[i];
 		}
 		printf("%d\n", lc);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
