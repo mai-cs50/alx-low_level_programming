90e738327405a61997da1e834012f5029363dd5b
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 00:38:50 -0700

    d

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,24 +1,46 @@
 #include "3-calc.h"
+
 /**
- * main - check code
- * @argc: n
- * @argv: argument
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: 0
- *
-*/
-int main(int argc, char **argv)
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
 {
-	'int (*op_func)(int , int), a, b;
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
-		printf("error\n"), exit(98);
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	op_func = get_op_func(argv[2]);
-	if (!op_func)
-		printf("error\n"), exit(99);
-	printf("%d\n", op_func(a, b));
-	return (0);
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
+
+	return (0);
 }
