d09444db1048901be7af33497491cda9a0490664
mai-cs50 <maimohamed.1992@yahoo.com>
2024-08-17 01:53:49 -0700

    d

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,12 +21,10 @@ int (*get_op_func(char *s))(int, int)
 
     int i = 0;
 
-    while (ops[i].op != NULL)
+    for (i = 0; ops[i].op != NULL; i++)
     {
-        if (strcmp(s, ops[i].op) == 0)
-            return (ops[i].f);
-        i++;
+	    if (strcmp(s, ops[i].op) == 0)
+		    return (ops[i].f);
     }
     return (NULL);
 }
-
